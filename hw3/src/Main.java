849d021f0d5fe8531f3178f1cdb46a87cb269309
Roksolana <roksolanakravchyshy121@gmail.com>
2018-07-30 13:02:32 +0300

    hw3/. hw3/ was added

---
+++ hw3/src/Main.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //---------------------------------------------------------
+        System.out.println("---Task1---");
+        int [] arr = new int[10];
+        for (int i=0; i<arr.length; i++){
+            arr[i]=((int)(Math.random()*31)-15);
+            System.out.println("Element :" + arr[i]);
+        }
+        System.out.println("Sort arr++");
+        Arrays.sort(arr);
+        for(int i =0; i<arr.length; i++){
+        System.out.println(arr[i] + " ");
+        }
+        //----------------------------------------------------------
+        System.out.println("---Task2---");
+       List animalList = new ArrayList(6);
+        animalList.add("Cat");
+        animalList.add("Dog");
+        animalList.add("Eagle");
+        animalList.add("Rabbit");
+        animalList.add("Wolf");
+        animalList.add("Bear");
+        System.out.println(animalList);
+        Collections.sort(animalList);
+        System.out.println("Sort:");
+        System.out.println(animalList);
+        //------------------------------------------------------------
+        System.out.println("---Task4---");
+        System.out.println("---Task5---");
+  }
+  //---Task5---------------------------------------------------------
+public void EmptyCollection (){
+    List FList= new ArrayList();
+    if (FList.isEmpty()) throw new NullPointerException ("Current collection is empty !!!");
+    }
+   //---Task3---------------------------------------------------------
+    public enum  User {
+        DEV("Ira","123"),
+        DEV1("Oleg","456"),
+        DEV2("Ivan","789"),
+        DEV3("Petro","000");
+
+        private  String name;
+        private  String pass;
+
+        User (final String name, final String pass){
+            this.name=name;
+            this.pass=pass;
+        }
+        /*
+   @BeforeTest
+   public void startPrint (){
+        System.out.println("Start printing user credentials");
+    }
+    @Test
+    public void printUserCredentials (){
+
+    }
+    @AfterTest
+    public void finishPrint (){
+        System.out.println("Finish printing user credentials");
+    }*/
+
+
+}}
